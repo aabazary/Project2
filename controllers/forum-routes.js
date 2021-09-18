188569f69796a1a8e8ed2cf6875d9df4a4f45e95
aabazary <aabazary@gmail.com>
2021-09-18 13:18:35 -0700

    a

---
+++ controllers/forum-routes.js
@@ -0,0 +1,174 @@
+const router = require('express').Router();
+const {
+    User,
+    Post,
+    Comment,
+    Game
+
+} = require('../models');
+// const withAuth = require('../utils/auth');
+
+
+router.get('/', async (req, res) => {
+    Post.findAll({
+            where: {
+                user_id: req.session.user_id
+            },
+            attributes: [
+                'id',
+                'title',
+                'content',
+                'createdAt'
+            ],
+            include: [{
+                    model: Comment,
+                    attributes: ['id', 'post_id', 'user_id', ],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+        .then(postData => {
+            const posts = postData.map(post => post.get({
+                plain: true
+            }));
+            res.render('dashboard', {
+                posts,
+                loggedIn: true
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/edit/:id', (req, res) => {
+    Post.findOne({
+            where: {
+                id: req.params.id
+            },
+            attributes: ['id',
+                'title',
+                'content',
+                'createdAt'
+            ],
+            include: [{
+                    model: User,
+                    attributes: ['username']
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'post_id', 'user_id', 'createdAt'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                }
+            ]
+        })
+        .then(postData => {
+            if (!postData) {
+                res.status(404).json({
+                    message: 'No post found'
+                });
+                return;
+            }
+
+            const post = postData.get({
+                plain: true
+            });
+            res.render('edit-post', {
+                post,
+                loggedIn: true
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+})
+
+router.get('/:id', (req, res) => {
+    Post.findOne({
+            where: {
+                id: req.params.id
+            },
+            attributes: ['id',
+                'title',
+                'content',
+                'createdAt'
+            ],
+            include: [{
+                    model: User,
+                    attributes: ['username']
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'post_id', 'user_id', 'createdAt'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                }
+            ]
+        })
+
+        .then(postData => res.json(postData))
+        
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+
+router.put('/:id', (req, res) => {
+    Post.update({
+            title: req.body.title,
+            content: req.body.content
+        }, {
+            where: {
+                id: req.params.id
+            }
+        }).then(postData => {
+            if (!postData) {
+                res.status(404).json({
+                    message: 'No post found'
+                });
+                return;
+            }
+            res.json(postData);
+            console.log(postData)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+router.delete('/:id', (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(postData => {
+        if (!postData) {
+            res.status(404).json({
+                message: 'No post found'
+            });
+            return;
+        }
+        res.json(postData);
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
+const forumRoutes = require('./home-routes.js');
 
+router.use('/forum', apiRoutes);
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 
