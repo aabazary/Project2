6b24b4a876570cde8b94064b591a5e1fba30eaa4
aabazary <85041715+aabazary@users.noreply.github.com>
2021-09-22 21:50:40 -0700

    fixed layout and routing of forum/post page

--- controllers/api/forum-routes.js
+++ controllers/api/forum-routes.js
@@ -4,7 +4,7 @@ const { User, Comment, Post } = require('../../models');
 
 
 
-// Post a new post
+// Post Comment
 router.post('/new', async (req, res) => {
 
     console.log('trying to create new post')
@@ -40,7 +40,22 @@ router.get('/:id', /* enable later withAuth, */ async (req, res) => {
             {
                 where: {
                     game_id: req.params.id
-                }
+                },
+                include: [
+                    {
+                        model: Comment,
+                        attributes: [
+                            'body'
+                        ]
+                    },
+                    {
+                        model: User,
+                        attributes: [
+                            'id',
+                            'username',
+
+                        ]
+                    }]
             },
         );
         const posts = postData.map((postInfo) =>
@@ -53,7 +68,7 @@ router.get('/:id', /* enable later withAuth, */ async (req, res) => {
 
 
         res.render('forum-landing-page-main', {
-            posts,
+            posts: posts,
             loggedIn: req.session.loggedIn
         });
     } catch (err) {
@@ -79,7 +94,16 @@ router.get('/post/:id', /* enable later withAuth, */ async (req, res) => {
                         attributes: [
                             'body'
                         ]
-                    }
+                    },
+                    {
+                        model: User,
+                        attributes: [
+                            'id',
+                            'username',
+
+                        ]
+                    },
+                    
                 ]
             }
         );
@@ -89,7 +113,7 @@ router.get('/post/:id', /* enable later withAuth, */ async (req, res) => {
 
 
         res.render('post-page', {
-            posts,
+            posts: posts,
             loggedIn: req.session.loggedIn
         });
     } catch (err) {
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const {
+    Game,
     User,
     Post,
     Comment
